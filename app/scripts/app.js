8358c6c3ae6f8fe8d4d987501c6bfe6d7bc78d4a
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2015-07-15 19:58:34 +0300

    Added angular

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adevApp
+ * @description
+ * # adevApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adevApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
